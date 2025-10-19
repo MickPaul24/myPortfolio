1f975857b50f4338767a296a616e448afff2a814
MickPaul24 <mickalexan24@gmail.com>
2025-10-19 14:09:58 +0300

    commit

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-    const showBtn = document.querySelector(".showButton");
-    const projectD = document.querySelector(".projectDes");
+    const showBtn = document.querySelector(".showButton1");
+    const showBtn2 = document.querySelector(".showButton2");
+    const projectD = document.querySelector(".projectDes1");
+    const projectD2 = document.querySelector(".projectDes2");
 
     showBtn.addEventListener("click",()=>{
       showBtn.classList.toggle("active");
       projectD.classList.toggle("active");
+      // projectD2.classList.toggle("active");
     })
 
-    document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",() =>{
-      showBtn.classList.remove("active");
-      projectD.classList.remove("active");
-    }))
+    showBtn2.addEventListener("click",()=>{
+      showBtn.classList.toggle("active");
+      // projectD.classList.toggle("active");
+      projectD2.classList.toggle("active");
+    })
+
+    // document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",() =>{
+    //   showBtn.classList.remove("active");
+    //   projectD.classList.remove("active");
+    // }))
