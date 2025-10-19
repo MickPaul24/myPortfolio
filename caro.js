cba28b1106f4579ba60eed49f84a2f9927acff6a
MickPaul24 <mickalexan24@gmail.com>
2025-10-19 12:52:07 +0300

    commit

---
+++ caro.js
@@ -0,0 +1,21 @@
+const track = document.querySelector(".reviewcarousel-track");
+const items = document.querySelectorAll(".carousel-item");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+let index = 0;
+
+function updateCarousel() {
+  const itemWidth = items[0].clientWidth;
+  track.style.transform = `translateX(-${index * itemWidth}px)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  index = (index + 1) % items.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+  index = (index - 1 + items.length) % items.length;
+  updateCarousel();
+});
