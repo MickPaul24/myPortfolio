4d40b7e4aeb77ceb20c7013aff533396877ee76d
MickPaul24 <mickalexan24@gmail.com>
2025-12-25 14:37:49 +0300

    gi

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ prevBtn.addEventListener("click", () => {
   updateCarousel();
 });
 //reviews navigation button js
+
+
+
+
+
+
+
+
+//navbar hamburger menu navbar hamburger menu navbar hamburger menu
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburger.addEventListener("click",()=>{
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    })
+
+    document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",() =>{
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    }))
