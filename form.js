50c3ecbd7077f48816ea6544c89460b8e5102d20
MickPaul24 <mickalexan24@gmail.com>
2025-12-29 19:03:23 +0300

    commit

--- form.js
+++ form.js
@@ -43,15 +43,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-      //smoothscroll animation
-  const observer2 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        console.log(entry.target)
-        entry.target.classList.add('show')}
-      else {
-        entry.target.classList.remove('show')}
-      })
-    },{})
-    const todoElements2 = document.querySelectorAll('.stat1');
-    todoElements.forEach((el) => observer.observe(el));
